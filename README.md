a0daf8a0c56a7015c67153a97f26a5821da8223f
Ward Liu <wardliu@seismic.com>
2024-12-12 20:47:48 +0800

    Init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# docker-image-sync
+# Docker 镜像同步
+
+将 docker.io 的 镜像同步到 阿里云的镜像服务中。可以解决 docker.io 被封的问题。
---
+++ sync_images.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+declare -A images
+images=(
+    ["postgres:latest"]="postgres:latest"
+    ["mysql:latest"]="mysql:latest"
+    ["redis:latest"]="redis:latest"
+    ["influxdb:latest"]="influxdb:latest"
+    ["mongo:latest"]="mongo:latest"
+    ["dbeaver/cloudbeaver:latest"]="cloudbeaver:latest"
+    ["dpage/pgadmin4:latest"]="pgadmin4:latest"
+    ["mongo-express:latest"]="mongo-express:latest"
+    ["gitlab/gitlab-ce:latest"]="gitlab-ce:latest"
+    ["nginx:latest"]="nginx:latest"
+    ["jenkins/jenkins:latest"]="jenkins:latest"
+    ["minio/minio:latest"]="minio:latest"
+    ["apache/kafka:latest"]="kafka:latest"
+    ["mcr.microsoft.com/dotnet/sdk:8.0"]="dotnet-sdk:8.0"
+    ["mcr.microsoft.com/dotnet/sdk:9.0"]="dotnet-sdk:9.0"
+    ["mcr.microsoft.com/dotnet/aspnet:8.0"]="dotnet-aspnet:8.0"
+    ["mcr.microsoft.com/dotnet/aspnet:9.0"]="dotnet-aspnet:9.0"
+    ["node:latest"]="node:latest"
+    ["ossrs/srs:latest"]="srs:latest"
+    ["mikolatero/vlmcsd:latest"]="mikolatero-vlmcsd:latest"
+    ["vlmcsd/vlmcsd:latest"]="vlmcsd:latest"
+    ["jellyfin/jellyfin:latest"]="jellyfin:latest"
+)
+
+registry="${REGISTRY}"
+
+for image in "${!images[@]}"; do
+    target=${registry}${images[$image]}
+    echo "Processing $image ---> $target"
+    docker pull $image
+    docker tag $image $target
+    docker push $target
+    echo "Complated $image ---> $target"
+done
