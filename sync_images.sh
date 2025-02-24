9c546f6d18aaa7179e147e8f8119a307ce8abc5d
Ward Liu <wardliu@seismic.com>
2025-02-24 20:24:20 +0800

    Update sync_images.sh

--- sync_images.sh
+++ sync_images.sh
@@ -6,27 +6,36 @@ images=(
     ["redis:latest"]="redis:latest"
     ["influxdb:latest"]="influxdb:latest"
     ["mongo:latest"]="mongo:latest"
+    ["docker.elastic.co/elasticsearch/elasticsearch:8.17.0"]="elasticsearch:8.17.0"
+    
+    ["docker.elastic.co/kibana/kibana:8.17.0"]="kibana:8.17.0"
     ["dbeaver/cloudbeaver:latest"]="cloudbeaver:latest"
     ["dpage/pgadmin4:latest"]="pgadmin4:latest"
     ["mongo-express:latest"]="mongo-express:latest"
+    
     ["gitlab/gitlab-ce:latest"]="gitlab-ce:latest"
     ["nginx:latest"]="nginx:latest"
     ["caddy:latest"]="caddy:latest"
     ["jenkins/jenkins:latest"]="jenkins:latest"
+    
     ["minio/minio:latest"]="minio:latest"
     ["apache/kafka:latest"]="kafka:latest"
+    ["rabbitmq:latest"]="rabbitmq:latest"
+    ["exceptionless/exceptionless:latest"]="exceptionless:latest"
+    
     ["mcr.microsoft.com/dotnet/sdk:8.0"]="dotnet-sdk:8.0"
     ["mcr.microsoft.com/dotnet/sdk:9.0"]="dotnet-sdk:9.0"
     ["mcr.microsoft.com/dotnet/aspnet:8.0"]="dotnet-aspnet:8.0"
     ["mcr.microsoft.com/dotnet/aspnet:9.0"]="dotnet-aspnet:9.0"
     ["node:latest"]="node:latest"
+    
     ["ossrs/srs:latest"]="srs:latest"
     ["mikolatero/vlmcsd:latest"]="mikolatero-vlmcsd:latest"
     ["vlmcsd/vlmcsd:latest"]="vlmcsd:latest"
     ["jellyfin/jellyfin:latest"]="jellyfin:latest"
-    ["docker.elastic.co/elasticsearch/elasticsearch:8.17.0"]="elasticsearch:8.17.0"
-    ["docker.elastic.co/kibana/kibana:8.17.0"]="kibana:8.17.0"
-    ["exceptionless/exceptionless:latest"]="exceptionless:latest"
+    ["ollama/ollama:latest"]="ollama:latest"
+    ["registry:latest"]="registry:latest"
+    ["consul:latest"]="consul:latest"
 )
 
 registry="${REGISTRY}"
