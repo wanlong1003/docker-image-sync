4acfaa21c71a920c421e099c3d08f37312ffed53
Ward Liu <wardliu@seismic.com>
2025-05-15 13:27:43 +0800

    Update manually_sync_images.sh

--- manually_sync_images.sh
+++ manually_sync_images.sh
@@ -4,8 +4,13 @@ namespace="${NAMESPACE}"
 image_name="${IMAGENAME}"
 image_version="${IMAGEVERSION}"
 
-source="${image_name}:{$image_version}"
+source="${image_name}:${image_version}"
 target="${registry}/${namespace}/${image_name}:${image_version}"
+
+echo "$registry"
+echo "$namespace"
+echo "$image_name"
+echo "$image_version"
 
 echo "Processing  $source ---> $target"
 docker pull $source
