4df6b4b430bdd9f759441025776389bdfff2992f
Ward Liu <wardliu@seismic.com>
2025-05-19 13:48:24 +0800

    Delete manually_sync_images.sh

--- manually_sync_images.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-source="${IMAGENAME}:${IMAGEVERSION}"
-target="${REGISTRY}/${NAMESPACE}/${IMAGENAME}:${IMAGEVERSION}"
-
-echo "Processing  $source ---> $target"
-docker pull $source
-docker tag $source $target
-docker push $target
-echo "Complated $source ---> $target"
